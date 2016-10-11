831c918bdaaceaf4ed88b8f66d630acbf3684861
costagilberto <costagilberto@users.noreply.github.com>
2016-10-11 15:27:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
                 <img src="Imagem1.jpg" alt="Academia de Tecnologia">
                 <p>Your PHP application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in your own dedicated environment in the AWS&nbsp;Cloud.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+                <p>Turma de 11/10/2016! Feliz dia das crianças!</p>
             </div>
         </div>
 
